918ef9b1cc8ab5d0b8c03d58f1c9f0cc1e3fe238
Steven <wusteven815@gmail.com>
2023-08-27 21:22:54 -0400

    feat: turn android navigation bar transparent

--- android/app/src/main/java/com/uwone/mobile/MainActivity.java
+++ android/app/src/main/java/com/uwone/mobile/MainActivity.java
@@ -1,7 +1,10 @@
 package com.uwone.mobile;
 
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,11 +16,16 @@ import expo.modules.ReactActivityDelegateWrapper;
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    // Set the theme to AppTheme BEFORE onCreate to support 
+    // Set the theme to AppTheme BEFORE onCreate to support
     // coloring the background, status bar, and navigation bar.
     // This is required for expo-splash-screen.
     setTheme(R.style.AppTheme);
-    super.onCreate(null);
+    Window w = getWindow();
+    w.setStatusBarColor(Color.TRANSPARENT);
+    w.setNavigationBarColor(Color.TRANSPARENT);
+    w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+    // setTheme(R.style.AppTheme);
+    super.onCreate(savedInstanceState);
   }
 
   /**
