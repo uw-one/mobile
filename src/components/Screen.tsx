35c2702cacec7c6a95ffcaabc819eed84d41ff56
Steven <wusteven815@gmail.com>
2023-08-30 17:00:10 -0400

    feat: screen - add actions and footer

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,17 +1,30 @@
 import React, { ReactNode } from "react";
-import { ScrollView, View } from "react-native";
+import { View } from "react-native";
 import { Appbar } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 type ScreenProps = {
+  actions?: {
+    icon: string;
+    onPress: () => void;
+  }[];
   classNames?: string;
+  footer?: ReactNode;
   name: string;
   noHeader?: boolean;
   noPadding?: boolean;
   children: ReactNode;
 };
 
-export function Screen({ classNames, name, noHeader, noPadding, children }: ScreenProps) {
+export function Screen({
+  actions,
+  classNames,
+  footer,
+  name,
+  noHeader,
+  noPadding,
+  children,
+}: ScreenProps) {
   if (noHeader) {
     return (
       <SafeAreaView className={`${!noPadding && "w-full flex-1 px-4"} ${classNames}`}>
@@ -21,15 +34,15 @@ export function Screen({ classNames, name, noHeader, noPadding, children }: Scre
   }
 
   return (
-    <ScrollView
-      showsHorizontalScrollIndicator={false}
-      showsVerticalScrollIndicator={false}
-      className="min-h-full w-full"
-    >
+    <View className="min-h-full w-full">
       <Appbar.Header mode="small">
         <Appbar.Content title={name} />
+        {(actions ?? []).map(({ icon, onPress }) => (
+          <Appbar.Action icon={icon} onPress={onPress} key={icon} />
+        ))}
       </Appbar.Header>
-      <View className={`${!noPadding && "px-4"} ${classNames}`}>{children}</View>
-    </ScrollView>
+      <View className={`flex-1 ${!noPadding && "px-4"} ${classNames}`}>{children}</View>
+      {footer}
+    </View>
   );
 }
